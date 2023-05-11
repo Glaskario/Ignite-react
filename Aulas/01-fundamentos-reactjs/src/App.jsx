ef2b8ae1c2e91b9f616425425d54766257db467b
Glaskario <shvavzhjnsbsjsjsjsj@gmail.com>
2023-05-11 08:08:22 -0300

    aaa

--- Aulas/01-fundamentos-reactjs/src/App.jsx
+++ Aulas/01-fundamentos-reactjs/src/App.jsx
@@ -6,29 +6,65 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/103462000?v=4',
+      name: 'Joao Marcelo',
+      role: 'Analista de sistemas.'
+    },
+    content: [
+        {type: 'paragraph', content: 'Fala galeraa 👋'},
+        {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare'},
+        {type: 'link', content: 'jane.design/doctorcare'},      
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/103462000?v=4',
+      name: 'Pedro Cavalcante',
+      role: 'Engenheiro de software e CTO @explorer_core '
+    },
+    content: [
+        {type: 'paragraph', content: 'Fala galeraa 👋'},
+        {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare'},
+        {type: 'link', content: 'jane.design/doctorcare'},      
+    ],
+    publishedAt: new Date('2022-05-07 21:00:00'),
+    
+  },
+];
+//author:{avatar_url:"", name: "", role: ""}
+//publishedAt: Date
+//content: String 
 
 export function App() {
- 
-
   return (
-    <>
+    <div>
     
       <Header />
 
       <div className={styles.wrapper}>
-      <Sidebar />
+        <Sidebar />
         <main>
-           <Post
-            author="Diego Fernandes"
-            content="  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, deserunt repellat eaque dolorem molestias hic in odit reiciendis est nulla tenetur consectetur expedita corporis facilis facere illo eius fugiat explicabo?"
-            />
-            <Post
-            author="Joao Aquino"
-            content="  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, deserunt repellat eaque "
-            /></main>
+          {post.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+            
+              />
+          )
+          })} 
+        </main>
       </div>
     
-    </>
+    </div>
   )
 }
 
